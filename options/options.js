aa5fc0c99710488bba578d3d8a934f66eb056234
Crowdbotics <team@crowdbotics.com>
2023-01-18 03:57:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "venti-medical-38335",
+  url: "https://venti-medical-38335.botics.co",
+  api: "https://venti-medical-38335.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
